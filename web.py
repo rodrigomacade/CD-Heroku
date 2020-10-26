6e21f3b685a32543bef257af1af3d25b6851121f
Rodrigo Macade <49011043+rodrigomacade@users.noreply.github.com>
2020-10-26 09:42:51 -0300

    Update web.py

--- web.py
+++ web.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Meu Segundo Pipeline - DevOps"
+    return "Meu Segundo Pipeline - DevOps Promote test"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
